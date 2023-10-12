c9de1b35af823dc94d5a7d2e01d73bf0fe301d49
Artisteo <hiartisteo@gmail.com>
2023-10-13 00:04:41 +0200

    feat: test husky

--- apps/artists/core/modules/debug-info/debug-info.ts
+++ apps/artists/core/modules/debug-info/debug-info.ts
@@ -1,8 +1,8 @@
 class DebugInfo {
-  public readonly enviroment: string;
+  public readonly NODE_ENV: string;
   public readonly NEXT_PUBLIC_DOT_ENV: string | undefined;
   constructor() {
-    this.enviroment = process.env.NODE_ENV;
+    this.NODE_ENV = process.env.NODE_ENV;
     this.NEXT_PUBLIC_DOT_ENV = process.env.NEXT_PUBLIC_DOT_ENV;
   }
 }
