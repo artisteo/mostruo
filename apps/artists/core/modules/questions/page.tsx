0d97652e70fe47d652b44035be9403424538f7ed
Artisteo <hiartisteo@gmail.com>
2023-10-14 00:22:38 +0200

    feat: remove debug from questions page

--- apps/artists/core/modules/questions/page.tsx
+++ apps/artists/core/modules/questions/page.tsx
@@ -1,11 +1,9 @@
-import DebugInfoDisplay from "../debug-info/debug-info-display";
 import QuestionsList from "./components/questions-list";
 import QuestionForm from "./components/question-form";
 
 function QuestionsPage(): JSX.Element {
   return (
     <main>
-      <DebugInfoDisplay />
       <h1>Questions</h1>
       <QuestionsList />
       <QuestionForm />
