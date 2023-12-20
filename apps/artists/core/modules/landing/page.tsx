eac707224fd86c879f45ee0b65c28c47abceb62a
artisteo <146995468+artisteo@users.noreply.github.com>
2023-12-21 01:18:10 +0400

    feat: add enviroment display by default

--- apps/artists/core/modules/landing/page.tsx
+++ apps/artists/core/modules/landing/page.tsx
@@ -3,7 +3,6 @@ import EnvironmentDisplay from "./environment-display";
 function LandingPage(): JSX.Element {
   return (
     <main>
-      <EnvironmentDisplay />
       <h1>Landing</h1>
     </main>
   );
