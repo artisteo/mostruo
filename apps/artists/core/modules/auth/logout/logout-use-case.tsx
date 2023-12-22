a0f8b4c039376071b06bed0c3ff76f4abc26bd97
artisteo <146995468+artisteo@users.noreply.github.com>
2023-12-22 05:17:21 +0400

    feat: add use cases

--- apps/artists/core/modules/auth/logout/logout-use-case.tsx
+++ apps/artists/core/modules/auth/logout/logout-use-case.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable no-console -- ^^ */
 import { NextResponse } from "next/server";
 import deleteServerCookie from "../cookie/delete-server-cookie";
 
 const logoutUseCase = (): NextResponse => {
-  console.log("🔥 ~ logoutUseCase:");
   deleteServerCookie();
   return NextResponse.json({}, { status: 200 });
 };
