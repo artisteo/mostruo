97fc0717321e5dedbe6f896f80a3f7895cfe40ca
artisteo <146995468+artisteo@users.noreply.github.com>
2023-12-24 01:09:19 +0400

    feat: add register route

---
+++ apps/artists/app/api/auth/register/route.ts
@@ -0,0 +1,6 @@
+import LoginGetController from "../../../../core/modules/auth/login/endpoints/login-get-controller";
+import LogoutPostController from "../../../../core/modules/auth/login/endpoints/login-post-controller";
+
+const GET = LoginGetController;
+const POST = LogoutPostController;
+export { GET, POST };
