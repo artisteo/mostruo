d92d277acc4bd5b41fce9a498d4aea792f45546c
artisteo <146995468+artisteo@users.noreply.github.com>
2023-12-24 05:01:30 +0400

    peine

---
+++ apps/artists/app/api/auth/logout-test/route.ts
@@ -0,0 +1,6 @@
+import LogoutGetController from "../../../../core/modules/auth/logout/endpoints/logout-get-controller";
+import LogoutPostController from "../../../../core/modules/auth/logout/endpoints/logout-post-controller";
+
+const GET = LogoutGetController;
+const POST = LogoutPostController;
+export { GET, POST };
