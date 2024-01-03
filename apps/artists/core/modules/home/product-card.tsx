7efd848c3cbeb3fc1dd376053e71e5dfbf661786
artisteo <146995468+artisteo@users.noreply.github.com>
2024-01-03 20:11:13 +0400

    feat: lint

--- apps/artists/core/modules/home/product-card.tsx
+++ apps/artists/core/modules/home/product-card.tsx
@@ -3,15 +3,17 @@ function ProductCard(): JSX.Element {
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
         <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
           alt="Shoes"
+          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">Shoes!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary" type="button">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
